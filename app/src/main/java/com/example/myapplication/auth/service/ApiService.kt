022123cf473cf94addb2fe894140b780b72aacf7
1Dohyeon <dh1072005@gmail.com>
2024-05-26 22:11:38 +0900

    fetch: json 형태로 요청하도록 수정

--- app/src/main/java/com/example/myapplication/auth/service/ApiService.kt
+++ app/src/main/java/com/example/myapplication/auth/service/ApiService.kt
@@ -3,12 +3,13 @@ package com.example.myapplication.auth.service
 import com.example.myapplication.auth.request.LoginRequest
 import retrofit2.Call
 import retrofit2.http.Body
+import retrofit2.http.Headers
 import retrofit2.http.POST
 
-data class LoginRequest(val username: String, val password: String)
 data class LoginResponse(val token: String)
 
 interface ApiService {
+    @Headers("Content-Type: application/json")
     @POST("api/v1/auth/login")
     fun login(@Body request: LoginRequest): Call<LoginResponse>
 }
