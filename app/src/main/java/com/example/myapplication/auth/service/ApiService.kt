4812c186bbf8b5ebd76fe97722a48d86fbf74b6b
1Dohyeon <dh1072005@gmail.com>
2024-05-28 20:22:59 +0900

    fetch: 응답 타입 지정

--- app/src/main/java/com/example/myapplication/auth/service/ApiService.kt
+++ app/src/main/java/com/example/myapplication/auth/service/ApiService.kt
@@ -1,12 +1,11 @@
 package com.example.myapplication.auth.service
 
 import com.example.myapplication.auth.request.LoginRequest
+import com.example.myapplication.auth.response.LoginResponse
 import retrofit2.Call
 import retrofit2.http.Body
 import retrofit2.http.Headers
 import retrofit2.http.POST
-
-data class LoginResponse(val token: String)
 
 interface ApiService {
     @Headers("Content-Type: application/json")
