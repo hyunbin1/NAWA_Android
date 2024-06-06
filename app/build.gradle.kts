bada7acff9f953f93060c91c2ca245034cc42b13
1Dohyeon <dh1072005@gmail.com>
2024-06-06 11:43:24 +0900

    [Add] 카카오 로그인 디펜던시 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,4 +54,5 @@ dependencies {
     implementation("androidx.room:room-runtime:2.6.1")
     kapt ("androidx.room:room-compiler:2.6.1")
     implementation("androidx.room:room-ktx:2.6.1")
+    implementation ("com.kakao.sdk:v2-user:2.20.1") // 카카오 로그인 API 모듈
 }
