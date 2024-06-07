d3649742c4d82007e7ad3502a3e8aaf7c3b16541
hyunbin1 <kimgusqls1@gmail.com>
2024-06-08 03:18:18 +0900

    [ADD] 회원 정보 수정 컬럼 추가

--- app/src/main/java/com/example/myapplication/data/DTO/Request/MemberUpdateRequest.kt
+++ app/src/main/java/com/example/myapplication/data/DTO/Request/MemberUpdateRequest.kt
@@ -1,10 +1,26 @@
 package com.example.myapplication.data.DTO.Request
 
-data class MemberUpdateRequest (
-    val signUpStep: String,
-    val role: String,
-    val profileImage: String,
-    val nickname: String
-
-
+data class MemberUpdateRequest(
+    val signUpStep: String? = null,
+    val role: String? = null,
+    val profileImage: String? = null,
+    val nickname: String? = null,
+    val name: String? = null,
+    val phoneNumber: String? = null,
+    val age: Int? = null,
+    val gender: String? = null,
+    val job: String? = null,
+    val address: String? = null,
+    val detailAddress: String? = null,
+    val activeAddress: String? = null,
+    val mbti: String? = null,
+    val smoking: String? = null,
+    val drinking: String? = null,
+    val religion: String? = null,
+    val hobby: String? = null,
+    val introduce: String? = null,
+    val answer1: String? = null,
+    val answer2: String? = null,
+    val answer3: String? = null,
+    val answer4: String? = null
 )
