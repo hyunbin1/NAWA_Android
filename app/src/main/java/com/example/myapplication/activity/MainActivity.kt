3ea9f746df314b9597dedc2fcf58ed657a1f52cc
1Dohyeon <dh1072005@gmail.com>
2024-06-07 21:34:21 +0900

    [ADD] 공지사항 insert select 기능 추가

--- app/src/main/java/com/example/myapplication/activity/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/activity/MainActivity.kt
@@ -7,12 +7,10 @@ import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
-import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.myapplication.R
-import com.example.myapplication.activity.signUp.InitSignUpActivity
 import com.example.myapplication.data.AppDatabase
 import com.example.myapplication.data.model.Member
 import com.example.myapplication.data.model.Notice
@@ -22,7 +20,10 @@ import com.example.myapplication.adapter.ClubBannerAdapter
 import com.example.myapplication.adapter.LoginDialogFragment
 import com.example.myapplication.adapter.NoticeAdapter
 import com.example.myapplication.data.DTO.Request.ClubBannerDTO
+import com.example.myapplication.data.database.Notification
+import com.example.myapplication.data.database.enum.NotificationCategory
 import com.example.myapplication.data.database.toClubBannerRequest
+import com.example.myapplication.data.helper.NoticeDbHelper
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -152,33 +153,61 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun fetchNotices() {
-        val call = RetrofitClient.apiService.getNotices()
-        call.enqueue(object : Callback<List<Notice>> {
-            override fun onResponse(call: Call<List<Notice>>, response: Response<List<Notice>>) {
-                if (response.isSuccessful) {
-                    response.body()?.let { notices ->
-                        val limitedNotices = if (notices.size > 5) notices.take(5) else notices
+        val dbHelper = NoticeDbHelper(this)
+        val db = dbHelper.readableDatabase
+        val cursor = db.query(
+            NoticeDbHelper.TABLE_NAME,
+            null, // 모든 열을 선택
+            null, // 조건 없음
+            null, // 조건 값 없음
+            null, // 그룹핑하지 않음
+            null, // 그룹핑 조건 값 없음
+            "${NoticeDbHelper.COLUMN_CREATE_AT} DESC" // 최신 순으로 정렬
+        )
 
-                        limitedNotices.forEach { notice ->
-                            Log.d("MainActivity", "공지사항 ID: ${notice.id}")
-                        }
+        val notices = mutableListOf<Notification>()
+        with(cursor) {
+            while (moveToNext()) {
+                val id = getInt(getColumnIndexOrThrow(NoticeDbHelper.COLUMN_ID))
+                val noticeUUID = getString(getColumnIndexOrThrow(NoticeDbHelper.COLUMN_NOTICE_UUID))
+                val category = getString(getColumnIndexOrThrow(NoticeDbHelper.COLUMN_CATEGORY))
+                val title = getString(getColumnIndexOrThrow(NoticeDbHelper.COLUMN_TITLE))
+                val content = getString(getColumnIndexOrThrow(NoticeDbHelper.COLUMN_CONTENT))
+                val pinned = getInt(getColumnIndexOrThrow(NoticeDbHelper.COLUMN_PINNED)) > 0
+                val pinnedAt = getString(getColumnIndexOrThrow(NoticeDbHelper.COLUMN_PINNED_AT))
+                val viewCount = getInt(getColumnIndexOrThrow(NoticeDbHelper.COLUMN_VIEW_COUNT))
+                val createAt = getString(getColumnIndexOrThrow(NoticeDbHelper.COLUMN_CREATE_AT))
+                val updatedAt = getString(getColumnIndexOrThrow(NoticeDbHelper.COLUMN_UPDATED_AT))
 
-                        noticeAdapter.setNotices(limitedNotices)
-                    } ?: run {
-                        Log.e("MainActivity", "공지사항 응답이 없습니다.")
-                    }
-                } else {
-                    Log.e("MainActivity", "공지사항을 가져오지 못했습니다: ${response.code()} - ${response.message()}")
-                    Toast.makeText(this@MainActivity, "공지사항을 가져오지 못했습니다.", Toast.LENGTH_SHORT).show()
+                try {
+                    val notificationCategory = NotificationCategory.valueOf(category)
+                    val notice = Notification(
+                        id,
+                        noticeUUID,
+                        notificationCategory,
+                        title,
+                        content,
+                        pinned,
+                        pinnedAt,
+                        viewCount,
+                        createAt,
+                        updatedAt
+                    )
+                    notices.add(notice)
+                } catch (e: IllegalArgumentException) {
+                    Log.e("MainActivity", "Invalid category value: $category")
                 }
             }
+        }
+        cursor.close()
 
-            override fun onFailure(call: Call<List<Notice>>, t: Throwable) {
-                Log.e("MainActivity", "공지사항을 가져오는 중 오류가 발생했습니다: ${t.message}")
-                Toast.makeText(this@MainActivity, "공지사항을 가져오는 중 오류가 발생했습니다: ${t.message}", Toast.LENGTH_SHORT).show()
-            }
-        })
+        val limitedNotices = if (notices.size > 5) notices.take(5) else notices
+        limitedNotices.forEach { notice ->
+            Log.d("MainActivity", "공지사항 ID: ${notice.id}")
+        }
+        noticeAdapter.setNotices(limitedNotices)
     }
+
 
     private fun fetchMemberInfo() {
         val sharedPreferences = getSharedPreferences("MyAppPreferences", Context.MODE_PRIVATE)
