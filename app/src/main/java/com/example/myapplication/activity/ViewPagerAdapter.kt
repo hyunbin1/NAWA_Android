4fc5a258f4d31cbdc7f413188ffa9441484f99b7
hyunbin1 <kimgusqls1@gmail.com>
2024-06-08 18:02:58 +0900

    [Fetch] 파일 위치 변경

--- app/src/main/java/com/example/myapplication/activity/ViewPagerAdapter.kt
+++ app/src/main/java/com/example/myapplication/activity/ViewPagerAdapter.kt
@@ -1,8 +1,11 @@
-package com.example.nawa
+package com.example.myapplication.activity
 
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.example.nawa.ClubInfoFragment
+import com.example.nawa.ClubIntroFragment
+import com.example.nawa.ClubReviewFragment
 
 class ViewPagerAdapter(fragmentActivity: FragmentActivity) : FragmentStateAdapter(fragmentActivity) {
     override fun getItemCount(): Int {
@@ -11,8 +14,8 @@ class ViewPagerAdapter(fragmentActivity: FragmentActivity) : FragmentStateAdapte
     override fun createFragment(position: Int): Fragment {
         return when (position) {
             0 -> ClubIntroFragment()
-            1 -> ClubInfoFragment()
-            2 -> ClubReviewFragment()
+//            1 -> ClubInfoFragment()
+            1 -> ClubReviewFragment()
             else -> ClubInfoFragment()
         }
     }
